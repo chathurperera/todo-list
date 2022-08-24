3ead21fce10cc6462f5066e75b2d99d4551ce354
chathuperera <chtahuraperera007@gmail.com>
2022-08-24 23:43:38 +0530

    dep added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import styles from './App.module.scss'
 const App = () => {
   return (
-    <div>App</div>
+    <div className={styles.heading}>
+      
+    </div>
   )
 }
 
