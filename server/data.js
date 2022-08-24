50a5688904ef754e415f095b65b89b26d487bce4
chathuperera <chtahuraperera007@gmail.com>
2022-08-24 23:51:28 +0530

    routes added

---
+++ server/data.js
@@ -0,0 +1,2 @@
+const todo = ["Wash the dishes","Go to the Gym","Finish the application"]
+module.exports = todo;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const tasks = require('./routes/tasks');
+
+app.use('/api/v1/tasks',tasks);
+
+
 
 const PORT = 5000;
-
-
-
 app.listen(PORT, () => {
   console.log(`server is listening on ${PORT}...`);
 });
